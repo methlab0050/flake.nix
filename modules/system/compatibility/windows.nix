5fc0ad2779ac4e61896413b2095f793c89991f7d
meth <methlab006@gmail.com>
2026-08-07 22:57:21 -0600

    Removing winboat

--- modules/system/compatibility/windows.nix
+++ modules/system/compatibility/windows.nix
@@ -1,13 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
-
-  virtualisation.podman = {
-    enable = true;
-    dockerCompat = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    winboat
-  ];
 }
