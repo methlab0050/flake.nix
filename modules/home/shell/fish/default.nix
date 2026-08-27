0474a69c5997e95bbec3b731d3f8b7f1dc6ba49a
meth <methlab006@gmail.com>
2026-08-27 05:56:19 -0600

    feat(fish): Enable recursive symlink for fish conf

--- modules/home/shell/fish/default.nix
+++ modules/home/shell/fish/default.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}: {
   home.packages = [pkgs.fish];
 
-  home.file.".config/fish".source = ./config;
+  home.file.".config/fish" = {
+    source = ./config;
+    recursive = true;
+  };
 }
