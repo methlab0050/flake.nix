123254b64ec35fee9bd5d34f06bfb07b673695a6
meth <methlab006@gmail.com>
2026-09-24 22:54:48 -0600

    feat(devenv): Add co cli

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,12 @@
-{...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  packages = [
+    inputs.co.packages.${pkgs.stdenv.system}.default
+  ];
+
   # Git pre-commit hooks
   git-hooks.hooks = {
     alejandra.enable = true;
